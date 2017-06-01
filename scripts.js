8a990ba2fcbd0303e90a0daa43d339d96d7555bd
Josh Porter <porter.josh@hotmail.com>
2017-06-01 09:39:13 -0400

    corrected structure

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 var friends = ["Sandy", "Cory", "Chris", "Terry", "Ariana"];
 
 for (var i = 0; i < friends.length; i++) {
-    for (var lines = 99; lines >= 2; lines--) {
-        console.log(lines + " lines of code in the file, " + lines +
+    for (var lines = 99; lines > 0; lines--) {
+      
+     if (lines > 2){
+          console.log(lines + " lines of code in the file, " + lines +
             " lines of code; " + friends[i] + " strikes one out, " + 
             "clears it all out, " + (lines-1) + " lines of code in the file");
+        } else if(lines === 2){
+              console.log(lines + " lines of code in the file, " + lines +
+                " lines of code; " + friends[i] + " strikes one out, " + 
+                 "clears it all out, " + (lines-1) + " lines of code in the file");
+        } else {
+             console.log(lines + " line of code in the file, " + lines +
+                " line of code; " + friends[i] + " strikes one out, " + 
+                "clears it all out, " + (lines) + " no more lines of code left in the file");
+            console.log(" ");
+
+        }
     }
-    console.log("1 line of code in the file, 1 line of code; " + friends[i] + 
-    " strikes one out, clears it all out, no more lines of code in the file")
-    console.log(" ");
 }
